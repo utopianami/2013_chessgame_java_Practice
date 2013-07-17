82468f821cbf048069d7ff3d9ad8c2d16bd2f26f
youngnam <utopianami@gmail.com>
2013-07-18 01:08:38 +0900

    [연습문제 2-2] 작성일 : 2013.07.18_01:07 작성자 : 이영남 변경내용 	- 자바 식별자 추가 : 자바문자여부, 시작문자여부,소문자확인여부, 숫자확인여

--- test/pieces/CharacterTest.java
+++ test/pieces/CharacterTest.java
@@ -3,9 +3,35 @@ package pieces;
 import junit.framework.TestCase;
 
 public class CharacterTest extends TestCase {
+	
+	//isWhitespace
 	public void testWhitespace() throws Exception {
 		assertEquals(true, Character.isWhitespace('\n'));
 		assertTrue(Character.isWhitespace('\t'));
 		assertTrue(Character.isWhitespace(' '));
 	}
-} 
+	
+	
+	//식별자 
+	public void testIdentifier() throws Exception{
+		assertFalse(Character.isJavaIdentifierPart('^')); //'^' 자바문자가 아니다 
+		assertTrue(Character.isJavaIdentifierPart('$')); //'$'자바문자  
+		assertTrue(Character.isJavaIdentifierPart('k')); //'k'자바문자
+		assertTrue(Character.isJavaIdentifierStart('b')); // 'b'자바 시작문자 
+		assertFalse(Character.isJavaIdentifierStart('9'));// '9'자바 시작문자가 아니다  
+	}
+	
+	
+	//소문자 식
+	public void testIsLowerCase() throws Exception {
+		assertTrue(Character.isLowerCase('b')); // 'b'는 소문자 
+		assertTrue(Character.isLowerCase('d')); // 'd'는 소문자 
+		assertFalse(Character.isLowerCase('D')); //'D'는 소문자가 아니다 
+	}
+	
+	//숫자인지 판별 
+	public void testISDigit() throws Exception {
+		assertTrue(Character.isDigit('6')); //6은 숫자이다. 
+		assertFalse(Character.isDigit('D')); //D는 숫자가 아니다 
+	}
+}
